0cd4f518d76d292117f0b998f301c58a0f8f80c5
Buggate <kayurdesalawu@gmail.com>
2017-10-06 18:40:45 +0100

    repo_heroku_new

--- app/models/category.rb
+++
@@ -1,5 +0,0 @@
-class Category < ApplicationRecord
-
-	# belongs_to :party
-
-end
--- config/routes.rb
+++ config/routes.rb
@@ -7,95 +7,65 @@ Rails.application.routes.draw do
 
   resources :profiles  do
     collection { post :search, to: 'profiles#index' }
-
   end
-
-
-  
   resources :abouts, only: :show
   resources :how_it_works, only: :show
   resources :term_and_conditions, only: :show
   resources :privacies, only: :show
   resources :contacts, only: [:new, :create, :show]
-
   resources :operators, :except => [:show] do
     member do
       get :delete
     end
     resources :launches
   end
-
-
-
-  get 'staff', :to => 'access#menu'
-  get 'access/menu'
-  get 'access/login'
-  post 'access/attempt_login'
-  get 'access/logout'
-
-
- 
-  resources :parties do
+    resources :parties do
     resources :invites
     resources :requests
-
-    
-
   end
-  
-
-  resources :visitors do 
-   
+ resources :visitors do 
    resources :notifications
    resources :reservations
-   
-    resources :albums do
-      member do
-        put "like", to: "albums#upvote"
-        put "dislike", to: "albums#downvote"
-     end
+     resources :albums do
+        member do
+          put "like", to: "albums#upvote"
+          put "dislike", to: "albums#downvote"
+       end
    end
- 
-  resources :comments, except: [:index, :new, :show] do
-      member do
-      get :reply
-     end
-    end
-
-    get 'catalogues/register'
-    
+ resources :comments, except: [:index, :new, :show] do
+   member do
+    get :reply
   end
+end
+  get 'catalogues/register'
+end
 
-   get 'notice', to: 'notifications#index', as: 'notifications'
-   get 'parties/:id/edit_venue' => 'parties#edit_venue', :as => :edit_venue_parties
-   get 'parties/:id/edit_date' => 'parties#edit_date', :as => :edit_date_parties
-   get 'notifications/:id/link_through', to: 'notifications#link_through', as: :link_through
-   get 'profiles/:id/edit_picture' => 'profiles#edit_picture', :as => :edit_picture_profiles
-   get 'contact-me', to: 'messages#new', as: 'new_message'
-   post 'contact-me', to: 'messages#create', as: 'create_message'
-   get 'photo-album', to: 'catalogues#index', as: 'catalogues'
-   post 'photo-album', to: 'catalogues#create', as: 'create_catalogue'
-   delete 'delete_catalogue', to: "catalogues#delete_catalogue"
-   delete 'delete_all', to: 'catalogues#delete_all'
+ get 'staff', :to => 'access#menu'
+ get 'access/menu'
+ get 'access/login'
+ post 'access/attempt_login'
+ get 'access/logout'
+ get 'notice', to: 'notifications#index', as: 'notifications'
+ get 'parties/:id/edit_venue' => 'parties#edit_venue', :as => :edit_venue_parties
+ get 'parties/:id/edit_date' => 'parties#edit_date', :as => :edit_date_parties
+ get 'notifications/:id/link_through', to: 'notifications#link_through', as: :link_through
+ get 'profiles/:id/edit_picture' => 'profiles#edit_picture', :as => :edit_picture_profiles
+ get 'contact-me', to: 'messages#new', as: 'new_message'
+ post 'contact-me', to: 'messages#create', as: 'create_message'
+ get 'photo-album', to: 'catalogues#index', as: 'catalogues'
+ post 'photo-album', to: 'catalogues#create', as: 'create_catalogue'
+ delete 'delete_catalogue', to: "catalogues#delete_catalogue"
+ delete 'delete_all', to: 'catalogues#delete_all'
+ get 'admin', :to => 'access#menu'
+ get 'access/menu'
+ get 'access/login'
+ post 'access/attempt_login'
+ get 'access/logout'
 
+ get '/sitemap.xml.gz', to: redirect("https://#{ENV['S3_BUCKET_NAME']}.s3.amazonaws.com/sitemaps/sitemap.xml.gz"), as: :sitemap
 
+ root 'public#index'
 
-  get 'admin', :to => 'access#menu'
-  get 'access/menu'
-  get 'access/login'
-  post 'access/attempt_login'
-  get 'access/logout'
-
-  # routes.rb file
-  
-
-get '/sitemap.xml.gz', to: redirect("https://#{ENV['S3_BUCKET_NAME']}.s3.amazonaws.com/sitemaps/sitemap.xml.gz"), as: :sitemap
-
-
-
-
-
-   root 'public#index'
 
 
 end
